1e75f1b479474b927ef7cf2f1fde3ae3753eb313
seemafirdaus777 <51022174+seemafirdaus777@users.noreply.github.com>
2019-08-07 10:42:52 +0200

    Create script.js

---
+++ week2/script.js
@@ -0,0 +1,83 @@
+Hello World // English
+Hej Varlden //Swedish
+Hello Duniya //Urdu
+Nmaste Duniya // Hindi
+
+//2
+console.log('I'm awesome');
+syntax error - argument list 
+console.log("I'm awesome");
+
+//3
+var x;
+console.log("the value of x is:undefined" );
+
+console.log(x)
+ x = 6;
+console.log("the value of x is: 6");
+console.log(x);
+
+//Strings
+var y = "Hello World";
+console.log("the value of my string will be: Hello World");
+console.log(y);
+y = "Hi There";
+console.log("the value of my string is: Hi There");
+console.log(y);
+
+
+var z = 7.25;
+console.log(z);
+ var a = Math.round(z);
+ console.log(a);
+ var highestValue = Math.max(z,a);
+ console.log(highestValue);
+ 
+ 
+ 
+  var myArray = [];
+ console.log("the value of my array is:empty");
+ console.log(myArray);
+ var animals = ['bear','monkey','lion'];
+ console.log(animals);
+ animals.unshift("baby pig");
+ console.log(animals);
+ 
+ 
+ let myString = "this is a test";
+ console.log(myString);
+ var length = myString.length;
+ console.log(myString); 
+ 
+ var myInt = 6;
+ console.log("the value of my variable int is: "+myInt);
+ console.log(typeof myInt);
+ var myString = "Hello";
+  console.log("the value of my variable string is: " + myString);
+  console.log(typeof myString);
+  var myArray = [1,2,3,4];
+   console.log("the value of my variable array is: " + myArray);
+   console.log(typeof myArray);
+   var myFloat = 2.34;
+    console.log("the value of my variable float is: " + myFloat);
+	console.log(typeof myFloat);
+	if (typeof myInt == typeof myString)
+		console.log("same type");
+	else console.log("not the same type");
+	
+	
+	var x = 7;
+	x = x % 3
+	console.log(x);
+	
+	var x = 8;
+	x = x % 2
+	console.log(x);
+	
+	var x = 9;
+	x = x % 4
+	console.log(x);
+	
+	//Storing multiple arrays
+	var myArray = ['John',4,'firstName',cat];
+	console.log(myArray);
